fbf009a7236918bf10a3bbd6b34ba6288d87c879
Galuh-design <pramudyaanantagaluh@gmail.com>
2025-10-01 14:27:36 +0700

    ku beri baris 5 sebagai user 1

--- Test15.java
+++ Test15.java
@@ -1,5 +1,7 @@
 public class Test15 {
     public static void main(String[] args) {
-        System.out.println("Hi, saya telah berhaasil");
+        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, saya telah merubah file kita");
+        System.out.println("User 1 menambahkan baris");
     }
 }
